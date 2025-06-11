c72dc3b90df30d5c9472c5fa743b94083f65f4b3
Cipto Setiono <chsetiono91@gmail.com>
2025-06-11 15:22:24 +0700

    feat: display tv

--- app/Http/Controllers/DisplayController.php
+++ app/Http/Controllers/DisplayController.php
@@ -6,17 +6,75 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
 
 class DisplayController extends Controller
 {
     public function show(): Response{
 
+        //nanti diambil dari database Masjid Sesuai data User
         $mosqueName = 'Masjid Al-Falah';
-        $prayerTimes = [];
+        $address = "Banjarsari, Gajah";
+        $city = 'Demak';
+        $mosqueAddress = $address.', '.$city;
+        $country = 'Indonesia';
+        $method = 20;
+
+        $template = 'Modern';// nanti bisa ambil dari database setting, pilihannya sesuai templatye yang tersedia
+        $view = 'display/'.$template;
+        $runningText = 'Selamat datang di Masjid Al-Falah. Jaga kebersihan dan ketenangan masjid.'; // nanti bisa ambil dari database running_text
+        $embedVideoId = 'nJrzkPxTRbI'; // ID video youtube
         
-        return Inertia::render('display/Board', [
+
+        $response = Http::get("https://api.aladhan.com/v1/timingsByCity", [
+            'city' => $city,
+            'country' => $country,
+            'method' => $method,
+            'tune' => '3,3,3,3,3,3,3,3,3',
+        ]);
+
+        if ($response->failed()) {
+            // Bisa redirect atau tampilkan error khusus di UI
+            abort(500, 'Gagal mengambil data jadwal shalat');
+        }
+
+        $data = $response->json()['data'];
+
+        // Pilih dan map hanya key yang kita butuhkan, sekaligus ganti ke uppercase
+        $raw = $data['timings'];
+        $prayerTimes = [
+            'IMSAK'   => substr($raw['Imsak'], 0, 5),
+            'SUBUH'   => substr($raw['Fajr'], 0, 5),
+            'DZUHUR'  => substr($raw['Dhuhr'], 0, 5),
+            'ASHAR'   => substr($raw['Asr'], 0, 5),
+            'MAGHRIB' => substr($raw['Maghrib'], 0, 5),
+            'ISYA'    => substr($raw['Isha'], 0, 5),
+        ];
+
+         //Format tanggal Masehi ke “Rabu, 11 Juni 2025”
+        $gregorianDate = Carbon::parse($data['date']['gregorian']['date'])
+                            ->locale('id')
+                            ->isoFormat('dddd, D MMMM YYYY');
+
+        // 2) Ambil data Hijri dari API, contoh month.en = “Dhū al-Ḥijjah”
+        $hijri = $data['date']['hijri'];
+        $hijriDate = sprintf(
+            '%d %s %d H',
+            $hijri['day'],
+            $hijri['month']['en'],
+            $hijri['year']
+        );
+
+        
+        return Inertia::render( $view, [
+            'prayerTimes' => $prayerTimes,
+            'gregorianDate' =>  $gregorianDate,
+            'hijriDate' => $hijriDate,
             'mosqueName' => $mosqueName,
-            'prayerTimes' =>  $prayerTimes
+            'mosqueAddress' =>  $mosqueAddress,
+            'runningText' => $runningText,
+            'embedVideoId' => $embedVideoId,
+            'template' => 'classic', // nanti bisa ambil dari database setting
         ]);
     }
 }
